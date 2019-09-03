6ae7b00de39144ebddea9636e8b4a14d8cf87f58
DidanRahmana <44270191+DidanRahmana@users.noreply.github.com>
2019-09-03 13:48:04 +0700

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Badan Geologi Pusat Air Tanah Dan Geologi Lingkungan</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/bootstrap/css/style.css" rel="stylesheet">
+    <link rel="icon" href="../app/views/img/logo.png">
+</head>
+<div class="container-fluid">
+
+    <body style="background-color: seashell;">
+
+        <!-- row 1: Header -->
+        <div class="row">
+            <div class="col-md-12" style="background-color: orange">
+                <img src="../app/views/img/logoesdm.jpg" alt="logoedsm" width="600px;">
+                <h2 style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; position: center"></h2>
+            </div>
+        </div>
+</div>
+
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand navbar-dark bg-dark top">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Kegiatan
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Bidang Air Tanah</a>
+                    <a class="dropdown-item" href="#">Bidang Geologi Teknik</a>
+                    <a class="dropdown-item" href="#">Bidang Geologi Lingkungan</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Air Tanah
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Regulasi Tentang Air Tanah</a>
+                    <a class="dropdown-item" href="#">Potensi Air Tanah</a>
+                    <a class="dropdown-item" href="#">Pendayagunaan Air Tanah</a>
+                    <a class="dropdown-item" href="#">Konservasi Air Tanah / Pengendalian Air Tanah</a>
+                    <a class="dropdown-item" href="#">SOP Pelayanan Rekomendasi Teknis Air Tanah</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Geologi Lingkungan
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Regulasi Tentang Geologi Lingkungan</a>
+                    <a class="dropdown-item" href="#">Geologi Lingkungan Untuk Penataan Ruang</a>
+                    <a class="dropdown-item" href="#">Geologi Lingkungan Regional</a>
+                    <a class="dropdown-item" href="#">Geologi Lingkungan Perkotaan</a>
+                    <a class="dropdown-item" href="#">Kawasan Karst</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Geologi Teknik
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Regulasi Tentang Geologi Teknik</a>
+                    <a class="dropdown-item" href="#">Inventarisasi Geologi Teknik</a>
+                    <a class="dropdown-item" href="#">Evaluasi Geologi Teknik</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Publikasi
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Buletin Geologi Tata Lingkungan</a>
+                    <a class="dropdown-item" href="#">Peta & Atlas</a>
+                    <a class="dropdown-item" href="#">Laporan Tahunan</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sarana & Prasarana
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Sarana dan Prasarana</a>
+
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+</div>
+</nav>
