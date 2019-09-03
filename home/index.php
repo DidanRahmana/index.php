5e65136ce72a53f13c407af602a8535879a797e8
DidanRahmana <44270191+DidanRahmana@users.noreply.github.com>
2019-09-03 13:37:29 +0700

    Add files via upload

---
+++ home/index.php
@@ -0,0 +1,159 @@
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Call to Action Well -->
+    <div class="card text-white bg-secondary my-5 py-4 text-center">
+        <div class="card-body">
+            <p class="text-white m-0">Selamat Datang di, Pusat Air Tanah Dan Geologi Tata Lingkungan</p>
+        </div>
+    </div>
+
+    <!-- Carousel / slideshow -->
+    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-interval="10000">
+                <img src="foto1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-interval="2000">
+                <img src="foto1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="Maklumat.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!-- end of carousel -->
+
+    <!-- Heading Row -->
+    <div class="row align my-5">
+        <div class="col-lg-8 mb-5">
+            <div class="card" style="width: 48rem; height: 40rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Indeks Kepuasan Masyarakat</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- /.col-lg-8 -->
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem; margin-left: 58px;">
+                <div class="card-body">
+                    <a href=""><img src="pengaduan.png" alt="pengaduan"></a>
+                </div>
+            </div>
+
+            <div class="card text-center" style="width: 18rem; margin-left: 58px;">
+                <div class="card-body">
+                    <a href="https://gol.kpk.go.id/login"><img src="gol-logo.png" alt="gol"></a>
+                </div>
+            </div>
+
+            <div class="card text-right" style="width: 18rem; margin-left: 58px;">
+                <div class="card-body">
+                    <center>
+                        <a href="https://wbs.esdm.go.id">
+                            <img src="kpk.jpg" alt="gol">
+                        </a>
+                    </center>
+                </div>
+            </div>
+
+            <div class="card text-center" style="width: 18rem; margin-left: 58px;">
+                <div class="card-body">
+                    <a href="https://wbs.esdm.go.id"><img src="korupsi.jpg" alt="gol"></a>
+                </div>
+            </div>
+
+            <div class="card text-center" style="width: 18rem; margin-left: 58px;">
+                <div class="card-body">
+                    <a href="https://wbs.esdm.go.id"><img src="jurnal.jpg" alt="gol"></a>
+                </div>
+            </div>
+
+            <div class="card text-center" style="width: 18rem; margin-left: 58px;">
+                <div class="card-body">
+                    <a href="https://wbs.esdm.go.id"><img src="hemat-air.png" alt="gol" width="200px;"></a>
+                </div>
+            </div>
+
+            <div class="card text-center" style="width: 18rem; margin-left: 58px;">
+                <div class="card-body">
+                    <a href="http://diko.geologi.esdm.go.id/"><img src="diko2.jpg" alt="gol" width="200px;"></a>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.col-md-4 -->
+
+        <div class="row align mb-4">
+            <div class="col-lg-8">
+                <div class="card" style="width: 48rem; height: 35rem;">
+                    <div class="card-body">
+                        <h4>Profile Pusat Air Tanah dan Geologi Tata Lingkungan</h4>
+                        <video width="720" height="480" controls>
+                            <source src="badangeologi.mp4">
+                        </video>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-md-4 mb-5">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Card One</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary btn-sm">More Info</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-md-4 -->
+            <div class="col-md-4 mb-5">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Card Two</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary btn-sm">More Info</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-md-4 -->
+            <div class="col-md-4 mb-5">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Card Three</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary btn-sm">More Info</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.col-md-4 -->
+
+    </div>
+    <!-- /.row -->
+</div>
+</div>
+<!-- /.container -->
